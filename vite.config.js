756a826b56295951614438e40b554d88717252a0
Santiago <santjf14@gmail.com>
2025-06-29 18:36:44 -0300

    fix: remove base config for netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-   plugins: [
-    tailwindcss(),
-  ],
+  // base: "/projeto-FinnApp/", // Linha removida ou comentada
   plugins: [react()],
 })
-
